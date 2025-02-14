d290273c29c975783fdd846a06f81d19b3bde78d
leafyin <chinayinye@gmail.com>
2025-02-14 23:06:51 +0800

    Update LOLTW-2Chinese.bat
    更新韩服、日服、美服汉化选项

--- LOLTW-2Chinese.bat
+++ LOLTW-2Chinese.bat
@@ -6,7 +6,19 @@ setlocal enabledelayedexpansion
 set filepath=C:\ProgramData\Riot Games\Metadata\league_of_legends.live
 set filename=C:\ProgramData\Riot Games\Metadata\league_of_legends.live\league_of_legends.live.product_settings.yaml
 set output_file=D:\league_of_legends.live.product_settings.yaml
+
+set /p user_input=请选择需要汉化的语言(0.繁体中文，1.日语，2.韩文，3.英文):
 set search_line=    locale: "zh_TW"
+if "%user_input%"=="1" (
+    set search_line=    locale: "ja_JP"
+)
+if "%user_input%"=="2" (
+    set search_line=    locale: "ko_KR"
+)
+if "%user_input%"=="3" (
+    set search_line=    locale: "en_US"
+)
+echo %search_line%
 set replace_line=    locale: "zh_CN"
 
 echo "确保你有D盘,该程序是用D盘存放临时文件"
