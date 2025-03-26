cb56e4a89c5608d1bc5fe949a329ff70d5047201
leafyin <chinayinye@gmail.com>
2025-03-26 10:01:35 +0800

    更新ui，改进用户体验，使用帮助

--- lol_cwa/gui.py
+++ lol_cwa/gui.py
@@ -14,7 +14,6 @@ import requests
 from pathlib import Path
 from tkinter import ttk, filedialog, messagebox
 from modelscope import snapshot_download
-from funasr import AutoModel
 
 app_id = '20250321002311115'
 private_key = '2CIieDd6J1OvEWpSyoej'
@@ -73,6 +72,7 @@ def translate(text, src, to):
 class SpeechModel:
 
     def __init__(self, model_dir):
+        from funasr import AutoModel
         # 加载实时流式语音识别模型
         self.model = AutoModel(
             model=model_dir,
@@ -108,7 +108,7 @@ class SpeechModel:
                 talk = res[0]['text']
                 if talk == '':
                     count += 1
-                    if count == 5:  # 5 * 600ms 设定3秒输出所说的话
+                    if count == 5:  # 5 * 600ms 设定可能3秒后输出所说的话
                         if len(result) != 0:
                             callback(result)
                             result = ''
@@ -137,6 +137,22 @@ class RedirectText:
         pass  # 必须定义 `flush()` 以兼容 `sys.stdout`
 
 
+def grid(component: tk.Widget, row, column, padx=5, pady=5, sticky='nsew', columnspan=None, rowspan=None):
+    """
+    表格布局
+    :return:
+    """
+    component.grid(
+        row=row,
+        column=column,
+        padx=padx,
+        pady=pady,
+        sticky=sticky,
+        rowspan=rowspan,
+        columnspan=columnspan,
+    )
+
+
 class AppGui:
 
     def __init__(self):
@@ -147,7 +163,6 @@ class AppGui:
         root = tk.Tk()
         root.title('语音识别翻译助手')
 
-        # 16:9长宽比
         screen_width = root.winfo_screenwidth()
         screen_height = root.winfo_screenheight()
         height = 500
@@ -160,24 +175,43 @@ class AppGui:
             # 启用音频监听
             model_name = '\\iic\\speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online'
             model_dir = save_path_entry.get()
+            if not model_dir:
+                messagebox.showwarning("警告", "请选择模型存放位置！")
+                return
             speech_model = SpeechModel(model_dir + model_name)
+            sys.stdout = RedirectText(log_text)
+            sys.stderr = RedirectText(log_text)
             threading.Thread(target=speech_model.audio_listener, args=(process_data,), daemon=True).start()
 
         # 启动按钮
-        onoff_btn = tk.Button(root, text='开启', command=onoff)
-        onoff_btn.pack(pady=10)
+        onoff_btn = tk.Button(root, text='启动', command=onoff)
+        onoff_btn.pack()
 
-        download_frame = tk.Frame(root)
-        download_frame.pack(expand=True, fill='both')
+        # frame
+        frame = tk.LabelFrame(root, text='配置', bd=2, relief="groove", padx=5, pady=10)
+        frame.pack()
 
-        # 模型下载存放位置
+        save_path_label = tk.Label(frame, text='模型保存路径：', font=self.font)
+        save_path_entry = tk.Entry(frame)
+        save_path_entry.config(state='readonly')
+
+        grid(save_path_label, row=0, column=0)
+        grid(save_path_entry, row=0, column=1)
+
+        # 模型存放位置
         def select_save_path():
             filepath = filedialog.askdirectory(
                 title='选择保存位置'
             )
+            save_path_entry.config(state='normal')
             save_path_entry.delete(0, tk.END)  # 清空输入框
             save_path_entry.insert(0, filepath)  # 插入用户选择的路径
+            save_path_entry.config(state='readonly')
 
+        select_btn = tk.Button(frame, text="选择", command=select_save_path)
+        grid(select_btn, row=0, column=2)
+
+        # 下载模型
         def download_model():
             save_path = save_path_entry.get()
             if not save_path:
@@ -190,23 +224,12 @@ class AppGui:
             sys.stderr = RedirectText(log_text)
             threading.Thread(target=download_model, daemon=True).start()
 
-        tk.Label(download_frame, text='模型保存路径：', font=self.font).pack(pady=10)
-        save_path_entry = tk.Entry(download_frame)
-        save_path_entry.pack(pady=10)
-        select_btn = tk.Button(download_frame, text="选择", command=select_save_path)
-        select_btn.pack(pady=10)
+        download_btn = tk.Button(frame, text='下载', command=start_download)
+        grid(download_btn, row=0, column=3)
 
-        download_btn = tk.Button(download_frame, text='下载模型', command=start_download)
-        download_btn.pack(pady=10)
-
-        log_text = tk.Text(download_frame, height=10, width=50)
-        log_text.pack(pady=10)
-
-        # frame
-        frame = tk.Frame(root)
-        frame.pack(expand=True, fill='both')
-
-        tk.Label(frame, text="要翻译成什么语言？", font=self.font).pack(pady=10)
+        # 选择语言
+        select_lang_label = tk.Label(frame, text="要翻译成什么语言？", font=self.font)
+        grid(select_lang_label, row=1, column=0)
 
         def on_select(event):
             reversed_lang = {v: k for k, v in LANG.items()}
@@ -216,15 +239,38 @@ class AppGui:
         for v in LANG.values():
             lang_name.append(v)
         combobox = ttk.Combobox(frame, values=lang_name)
-        combobox.pack(pady=10)
+        grid(combobox, row=1, column=1, columnspan=3)
         combobox.set('请选择')
+        combobox.config(state='readonly')
         combobox.bind('<<ComboboxSelected>>', on_select)
+
+        # frame2
+        frame2 = tk.LabelFrame(root, text='控制台输出', bd=2, relief="groove", padx=5, pady=10)
+        frame2.pack()
+
+        # 控制台输出
+        log_text = tk.Text(frame2, height=10, width=55)
+        log_text.pack()
+
+        # frame3
+        frame3 = tk.LabelFrame(root, text='使用帮助', bd=2, relief="groove", padx=5, pady=10)
+        frame3.pack()
+
+        help_text = tk.Text(frame3, height=10, width=55)
+        help_text.insert(
+            '1.0',
+            '1.第一次使用请先下载模型，选择模型保存路径，点击下载，下载进度在控制台输出，下载完成后控制台停止输出\n'
+            '2.选择需要翻译的语言，支持（英语、韩语、日语、俄语）翻译\n'
+            '3.上述操作完成之后点击启动，会等待数秒，启动后控制台会持续输出，此时麦克风音频已经开始监听，开始说话即可\n'
+            '4.如果已经下载好模型，请直接选择模型保存路径，一般是根目录，iic目录的上一级目录，再选择翻译的语言，点击启动\n'
+        )
+        help_text.config(state='disabled')
+        help_text.pack()
 
         # 浮动窗口，让窗口始终保持在最前，50%透明、移除标题栏
         window = tk.Toplevel(root)
         window.attributes('-topmost', True)
         window.attributes('-alpha', 0.8)
-        window.overrideredirect(True)
 
         source_text = tk.Entry(window, width=50, font=self.entry_font)
         source_text.pack()
@@ -239,10 +285,13 @@ class AppGui:
             translate_text.delete(0, tk.END)
             translate_text.insert(0, translate(data, 'zh', self.lang))
 
+        def send(event):
             keyboard.send('enter')
             time.sleep(0.05)
             keyboard.write(translate_text.get())
             keyboard.send('enter')
+
+        keyboard.on_press_key('enter', send)
 
         # 运行 Tkinter 主循环
         root.mainloop()
